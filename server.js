291c2df262483729ce421dddbdb94d347d6c8a7c
abhijeetsenguptaa <101327881+abhijeetsenguptaa@users.noreply.github.com>
2023-05-25 23:18:21 +0530

    json-server deploy

---
+++ server.js
@@ -0,0 +1,24 @@
+const jsonServer = require('json-server');
+
+const app = jsonServer.create()
+const route = jsonServer.router('db.json');
+const middleware = jsonServer.defaults();
+
+app.use(middleware);
+app.use(route);
+
+
+
+app.listen(8080,()=>{
+    console.log(`Server is running at the port 8080`);
+})
+// const jsonServer = require('json-server');
+// const server = jsonServer.create();
+// const router = jsonServer.router('data.json');
+// const middlewares = jsonServer.defaults();
+
+// server.use(middlewares);
+// server.use(router);
+// server.listen(3000, () => {
+//   console.log('JSON Server is running on port 3000');
+// });
