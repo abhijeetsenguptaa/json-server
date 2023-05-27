18576634d16fd31adb453f5cc4755872ee8c081b
abhijeetsenguptaa <101327881+abhijeetsenguptaa@users.noreply.github.com>
2023-05-27 08:57:04 +0530

    custom middleware

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const jsonServer = require('json-server');
-const cors = require('cors');
 const app = jsonServer.create()
 const route = jsonServer.router('db.json');
 const middleware = jsonServer.defaults();
 
-app.use(cors());
 app.use(middleware);
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.use(route);
 
 
 
-app.listen(8080,()=>{
+app.listen(8080, () => {
     console.log(`Server is running at the port 8080`);
 })
 // const jsonServer = require('json-server');
